098341773ef5a51a5b798548ca4925f45df4c27c
Mason Seale <masonseale88@gmail.com>
2016-12-14 20:47:32 -0500

    basic grid and api test

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
+import beerPic from './beer.jpg';
+
+const myHeaders = new Headers();
+
+const myInit = { method: 'GET',
+               headers: myHeaders,
+               };
+
+const styles = {
+  header: {
+    backgroundImage: 'url(' + beerPic + ')'
+  }
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      beers: [],
+      searchTerm: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api', {
+      accept: 'application/json'
+    })
+      .then(response => response.json())
+      .then(beers => {
+        console.log(beers);
+        this.setState({ beers });
+      })
+      .catch(err => {
+        console.log('There was an error fetching from API:' + err.message);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid fluid={true}>
+        <Row>
+          <Col sm={12}>
+            <Jumbotron style={styles.header}>
+              <h1>Hello World!</h1>
+              {/*<Search />*/}
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={7}>
+            {/*<BeerList />*/}
+          </Col>
+          <Col md={5}>
+            {/*<Map />*/}
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import Express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import chalk from 'chalk';
+import path from 'path';
+import request from 'request';
+
+const app = Express();
+
+const apiKey = '5fdb7de894515e9cc58fd0ca8fe9f5db';
+const testUrl = 'http://api.brewerydb.com/v2/beers/?key=' + apiKey + '&abv=8,10';
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/api', (req, res, next) => {
+    request.get(testUrl, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            res.json(body); 
+        } else {
+            console.log(chalk.red('Error retrieving from brewerydb api:', error));
+        }
+    });
+});
+
+app.listen(3001, () => {
+    console.log(chalk.cyan('Server listening on port 3001...'));
+});
