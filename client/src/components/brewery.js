72d876a7ae1b41fc021050384207e53aefa18c3e
Mason Seale <masonseale88@gmail.com>
2016-12-26 15:47:51 -0700

    collapsible panel functionality

--- client/src/components/brewery.js
+++ client/src/components/brewery.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
-import { Image, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router';
+import { Image, Row, Col, Panel, Button } from 'react-bootstrap';
 
 class Brewery extends Component {
+    constructor() {
+        super();
+        this.state = {
+            open: false
+        }
+    }
+
+    toggleDetails(e) {
+        e.preventDefault();
+        this.setState({open: !this.state.open});
+    }
 
     render() {
         return (
-            <a href="#" onClick={() => this.props.goToBrewery(this.props.brewery)} className="list-group-item">
-                <Row>
-                    <Col md={8}>
-                        <p>
-                            {this.props.brewery.name}
-                        </p>
-                        <p>
-                            {this.props.brewery.distance.toPrecision(2) + ' miles away'}
-                        </p>
-                    </Col>
-                    <Col md={4}>
-                        <Image src={this.props.brewery.imageIcon} />
-                    </Col>
-                </Row>
-            </a>
+            <div>
+                <a href="#" className="list-group-item">
+                    <Row>
+                        <Col md={7}>
+                            <p>
+                                {this.props.brewery.name}
+                            </p>
+                            <p>
+                                {this.props.brewery.distance.toPrecision(2) + ' miles away'}
+                            </p>
+                        </Col>
+                        <Col md={3}>
+                            <Image src={this.props.brewery.imageIcon} />
+                        </Col>
+                        <Col md={2}>
+                            <Button onClick={this.toggleDetails.bind(this)}>Details</Button>
+                        </Col>
+                    </Row>
+                </a>
+                <Panel collapsible expanded={this.state.open}>
+                    <p>{this.props.brewery.description}</p>
+                </Panel>
+            </div>
         );
     }
 }
